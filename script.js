6ff7d3fb89e655923644cc055473400d789005c9
Md.Saddam Hossain <csesolve6548@gmail.com>
2026-03-16 02:45:12 +0600

    Update by Character Counter
    HTML, CSS, Tailwind, Tailwind

---
+++ script.js
@@ -0,0 +1,64 @@
+const textInput = document.getElementById("textInput");
+const charCount = document.getElementById("charCount");
+const wordCount = document.getElementById("wordCount");
+const remaining = document.getElementById("remaining");
+const progressBar = document.getElementById("progressBar");
+const clearBtn = document.getElementById("clearBtn");
+const copyBtn = document.getElementById("copyBtn");
+const preview = document.getElementById("preview");
+const modeToggle = document.getElementById("modeToggle");
+
+const maxChars = 200; // Maximum character Limit
+
+//Update Counts and Progress
+textInput.addEventListener("input", () =>{
+	const text = textInput.value;
+	const length = text.length;
+	const words = text.trim() === "" ? 0 : text.trim().split(/\s+/).length;
+	
+	charCount.textContent = `${length} Characters`;
+	wordCount.textContent = `${words} Words`;
+	remaining.textContent = `Remaining : ${Math.max(maxChars - length, 0)}`;
+
+	const progress = Math.min((length / maxChars) * 100, 100);
+	progressBar.style.width = progress + "%";
+
+	//Change Progress Bar Color Based on limit
+	if (length > maxChars) {
+		progressBar.classList.remove("bg-blue-500");
+		progressBar.classList.add("bg-red-500");
+	}else{
+		progressBar.classList.remove("bg-red-500");
+		progressBar.classList.add("bg-blue-500");
+	}
+
+	//Live Preview
+	preview.textContent = text;
+});
+
+clearBtn.addEventListener("click", () =>{
+textInput.value = "";
+charCount.textContent = "0 Characters";
+wordCount.textContent = "0 Words";
+remaining.textContent = `Remaining: ${maxChars}`;
+progressBar.style.width = "0%";
+preview.textContent = "";
+});
+
+//Copy Button
+copyBtn.addEventListener("click", () =>{
+	navigator.clipboard.writeText(textInput.value).then(() =>{
+		alert("Copied to Clipboard!");
+	});
+});
+
+//Dark/Light Mode Toggle
+modeToggle.addEventListener('click', () =>{
+	document.body.classList.toggle("dark");
+	if(document.body.classList.contains("dark")){
+		modeToggle.textContent = "☀️";
+ 	}
+ 	else{
+ 		modeToggle.textContent = "🌙";
+ 	}
+});
